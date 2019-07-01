4e1f490ec9c3a3801e6036916d4dafad34cd0ab5
Neil Stoker <neil.stoker@gmail.com>
2019-07-01 11:13:50 +0100

    WiP setting version details

--- NewCoolTest/mainwindow.cpp
+++ NewCoolTest/mainwindow.cpp
@@ -1,14 +1,21 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QLabel>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    initialiseFooter();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::initialiseFooter(){
+    QLabel *versionLabel = new QLabel("APP_VERSION");
+    statusBar()->addWidget(versionLabel);
 }
--- NewCoolTest/mainwindow.h
+++ NewCoolTest/mainwindow.h
@@ -17,6 +17,8 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    void initialiseFooter();
 };
 
 #endif // MAINWINDOW_H
